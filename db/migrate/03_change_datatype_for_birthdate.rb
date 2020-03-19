aeebcd0cab0e2fab5e12f79c2bd8a9ae5b1dc7ad
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 02:24:17 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change_column
-    change_column :students, 
+    change_column :students, :birthdate, :datetime 
   end 
 end 
     
